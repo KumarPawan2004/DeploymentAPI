ac625fbf38ddcf814f9e02fd2c5bd2b70d50bfcc
Pawan Kumar Thakur <kumarthakurpawan788@gmail.com>
2026-05-15 06:35:45 +0530

    cors update

--- Program.cs
+++ Program.cs
@@ -50,8 +50,7 @@ builder.Services.AddCors(options =>
         builder => builder
             .AllowAnyOrigin()
             .AllowAnyMethod()
-            .AllowAnyHeader()
-            .AllowCredentials());
+            .AllowAnyHeader());
 });
 
 builder.Services.AddControllers();
